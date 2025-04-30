6d0619e60154ebb923b2260b2eed354c21a5de4e
Fizxxc <fizzx909@gmail.com>
2025-04-30 22:01:05 +0700

    driver

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static('public')); // Folder untuk file HTML dan statis lainnya
+
+let driverLocations = {}; // Menyimpan lokasi driver berdasarkan socket.id
+
+// Ketika ada koneksi dari client
+io.on('connection', (socket) => {
+  console.log('Client connected:', socket.id);
+
+  // Mendapatkan update lokasi dari driver
+  socket.on('driverLocationUpdate', (data) => {
+    driverLocations[socket.id] = {
+      lat: data.lat,
+      lng: data.lng,
+      name: data.name || `Driver ${socket.id}`,  // Jika nama kosong, gunakan ID socket
+      photo: data.photo || 'https://i.imgur.com/6b6psnA.png' // Default foto profil
+    };
+    io.emit('updateDriverLocations', driverLocations); // Mengirim update ke semua client
+  });
+
+  // Menghapus driver dari daftar saat disconnect
+  socket.on('disconnect', () => {
+    delete driverLocations[socket.id];
+    io.emit('updateDriverLocations', driverLocations); // Kirim update setelah driver disconnect
+  });
+});
+
+// Menjalankan server pada port 3000
+server.listen(3000, () => {
+  console.log('Server running at http://localhost:3000');
+});
